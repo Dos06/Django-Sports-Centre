c5cc56a73305b813f68ff4e8a8f8d9021278f163
Dos06 <dostermorgan815@gmail.com>
2019-11-22 01:17:34 +0600

    updated Front

--- user/views.py
+++ user/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import User
+# from django.template import loader
+from django.shortcuts import render
+# from .templates import ProjectPython
 
 
 # Create your views here.
@@ -13,11 +16,17 @@ def test(request):
 # a function to show User's homepage
 def index(request):
     all_users = User.objects.all()          # connect to DB, table users
-    html = ''
-    for user in all_users:
-        url = '/user/' + str(user.member_id) + '/'
-        html += '<a href="' + url + '">' + user.first_name + ' ' + user.last_name + '</a><br>'
-    return HttpResponse(html)
+    # template = loader.get_template('user/ProjectPython/Index.html')
+    context = {
+        'all_users': all_users,
+    }
+    return render(request, 'user/ProjectPython/Index.html', context)
+
+    # html = ''
+    # for user in all_users:
+    #     url = '/user/' + str(user.member_id) + '/'
+    #     html += '<a href="' + url + '">' + user.first_name + ' ' + user.last_name + '</a><br>'
+    # return HttpResponse(html)
 
 
 # a function to show details of a user
